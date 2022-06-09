0affa6fdd9eb5eef77a36d36a948d045678f4e8e
Abiodun Azeez <iamhabbeboy@gmail.com>
2022-06-09 23:56:33 +0100

    init

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func DoWork() int {
+	time.Sleep(time.Second)
+	return rand.Intn(100)
+}
+
+func main() {
+	dataChan := make(chan int)
+	go func() {
+		wg := sync.WaitGroup{}
+		for i := 0; i < 1000; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				dataChan <- DoWork()
+			}()
+		}
+		wg.Wait()
+		close(dataChan)
+	}()
+
+	for n := range dataChan {
+		fmt.Printf("n = %d\n", n)
+	}
+}
